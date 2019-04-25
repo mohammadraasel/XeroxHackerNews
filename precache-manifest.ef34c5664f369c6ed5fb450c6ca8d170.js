3247d3d0ddd817843cce8e63fe7e94a3ed37c456
Mohammmd Rasel <mohammadrasel1221@gmail.com>
2019-04-25 11:23:29 +0600

    Updates

---
+++ precache-manifest.ef34c5664f369c6ed5fb450c6ca8d170.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7cf5251badd84ad36ff",
+    "url": "/XeroxHackerNews/static/js/runtime~main.b3523195.js"
+  },
+  {
+    "revision": "941a2e10963b67f3c1ca",
+    "url": "/XeroxHackerNews/static/js/main.0e93ecae.chunk.js"
+  },
+  {
+    "revision": "3af040b1eae6c2fcc41f",
+    "url": "/XeroxHackerNews/static/js/2.a17bcd12.chunk.js"
+  },
+  {
+    "revision": "0475e6b891990303960cf53477d00959",
+    "url": "/XeroxHackerNews/index.html"
+  }
+];
